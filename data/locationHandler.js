d82755327ede4e94bee875a6a303a07b79655857
Elijah <plushabeststyle@gmail.com>
2026-06-09 23:08:31 +0300

    added locks, bashing, and guardians to dungeon doors

--- data/locationHandler.js
+++ data/locationHandler.js
@@ -6,22 +6,20 @@ import { AdventureLogHandler } from "./AdventureLogHandler.js";
 
 let adventureLogHandler = new AdventureLogHandler();
 
+let dungeonWindow = document.querySelector(".dungeon-box");
+let dungeonDescription = dungeonWindow.querySelector(".dungeon-description");
+
+let yesBtn = dungeonWindow.querySelector(".dungeon-door-button-yes");
+let noBtn = dungeonWindow.querySelector(".dungeon-door-button-no");
+let fightGuardianBtn = dungeonWindow.querySelector(".fight-guardian");
+let unlockBtn = dungeonWindow.querySelector(".unlock-button");
+let bashBtn = dungeonWindow.querySelector(".bash-button");
+
 export function checkDungeonAccess(id) {
     gameData.isEventActive = true;
-
-    let level = getLevel(id);
-    let dungeonWindow = document.querySelector(".dungeon-box");
     dungeonWindow.classList.remove("hidden");
-
-    let dungeonDescription = dungeonWindow.querySelector(".dungeon-description");
-
+    let level = getLevel(id);
     markLocationSeen({id: level.id, name: level.name, type: level.type});
-
-    let yesBtn = dungeonWindow.querySelector(".dungeon-door-button-yes");
-    let noBtn = dungeonWindow.querySelector(".dungeon-door-button-no");
-    let fightGuardianBtn = dungeonWindow.querySelector(".fight-guardian");
-    let unlockBtn = dungeonWindow.querySelector(".unlock-button");
-    let bashBtn = dungeonWindow.querySelector(".bash-button");
 
     yesBtn.onclick = () => {
         gameData.isEventActive = false;
@@ -36,66 +34,19 @@ export function checkDungeonAccess(id) {
 
     let isGuardianDefeated = gameData.eventOutcomes.find(e => e.event === level.isGuarded.id);
 
-    if (level.isGuarded && !isGuardianDefeated) {
-        dungeonDescription.textContent = "You found a " + level.type + " called " + level.name + ". It has a guardian." +
-            " Do you wish to fight them?";
-        fightGuardianBtn.classList.remove("hidden");
-        yesBtn.classList.add("hidden");
-
-        fightGuardianBtn.onclick = () => {
-            initCombat(level.isGuarded.id, level.isGuarded.enemyType);
-        };
-
-        document.addEventListener("combatEnded", () => {
-            isGuardianDefeated = gameData.eventOutcomes.find(e => e.event === level.isGuarded.id);
-            if (isGuardianDefeated) {
-                dungeonDescription.textContent = "You found a " + level.type + " called " + level.name + ". You have defeated its guardian." +
-                    " Do you wish to enter?"
-                fightGuardianBtn.classList.add("hidden");
-                yesBtn.classList.remove("hidden");
-            }
-        });
+    if (level.isGuarded && !isGuardianDefeated && level.isLocked) {
+        handleGuardian(isGuardianDefeated, level);
     }
 
-    if (level.isLocked) {
-        dungeonDescription.textContent = "You found a " + level.type + " called " + level.name + ". It's locked." +
-            " The lock level is " + level.isLocked;
-        unlockBtn.classList.remove("hidden");
-        bashBtn.classList.remove("hidden");
-        yesBtn.classList.add("hidden");
-
-        unlockBtn.onclick = () => {
-            let isUnlocked = pickLock(gameData.playerCharacteristics.agility, level.isLocked);
-            if (isUnlocked) {
-                level.isLocked = "";
-                yesBtn.classList.remove("hidden");
-
-                unlockBtn.classList.add("hidden");
-                bashBtn.classList.add("hidden");
-                adventureLogHandler.appendSystemMessage("You successfully unlocked the door.");
-                dungeonDescription.textContent = "You found a " + level.type + " called " + level.name + ". It's unlocked.";
-            } else {
-                adventureLogHandler.appendSystemMessage("You failed to unlock the door.");
-            }
-        }
-
-        bashBtn.onclick = () => {
-            let isBashed = bashDoor(gameData.playerCharacteristics.might, level.isLocked);
-            if (isBashed) {
-                level.isLocked = "";
-                yesBtn.classList.remove("hidden");
-
-                unlockBtn.classList.add("hidden");
-                bashBtn.classList.add("hidden");
-                adventureLogHandler.appendSystemMessage("You bashed the door with all your might.");
-                dungeonDescription.textContent = "You found a " + level.type + " called " + level.name + ". The door is destroyed.";
-            } else {
-                adventureLogHandler.appendSystemMessage("You are too weak to bash this door.");
-            }
-        }
+    else if (level.isGuarded && !isGuardianDefeated) {
+        handleGuardian(isGuardianDefeated, level);
     }
 
-    if (!level.isLocked && !level.isGuarded) {
+    else if (level.isLocked) {
+        handleLock(level);
+    }
+
+    else {
         dungeonDescription.textContent = "You found a " + level.type + " called " + level.name + ". Do you wish to enter?";
     }
 }
@@ -115,4 +66,65 @@ function pickLock(agility, lock) {
 
 function bashDoor(might, lock) {
     return (might + Math.floor(Math.random() * might)) > lock;
+}
+
+function handleGuardian(isGuardianDefeated, level) {
+    dungeonDescription.textContent = "You found a " + level.type + " called " + level.name + ". It has a guardian." +
+        " Do you wish to fight them?";
+    fightGuardianBtn.classList.remove("hidden");
+    yesBtn.classList.add("hidden");
+
+    fightGuardianBtn.onclick = () => {
+        initCombat(level.isGuarded.id, level.isGuarded.enemyType);
+    };
+
+    document.addEventListener("combatEnded", () => {
+        isGuardianDefeated = gameData.eventOutcomes.find(e => e.event === level.isGuarded.id);
+        if (isGuardianDefeated) {
+            fightGuardianBtn.classList.add("hidden");
+            if (level.isLocked) {
+                dungeonDescription.textContent = "You found a " + level.type + " called " + level.name + ". You have defeated its guardian." +
+                    " The door is locked." + " The lock level is " + level.isLocked;
+                handleLock(level);
+            } else {
+                yesBtn.classList.remove("hidden");
+            }
+        }
+    });
+}
+
+function handleLock(level) {
+    unlockBtn.classList.remove("hidden");
+    bashBtn.classList.remove("hidden");
+    yesBtn.classList.add("hidden");
+
+    unlockBtn.onclick = () => {
+        let isUnlocked = pickLock(gameData.playerCharacteristics.agility, level.isLocked);
+        if (isUnlocked) {
+            level.isLocked = "";
+            yesBtn.classList.remove("hidden");
+
+            unlockBtn.classList.add("hidden");
+            bashBtn.classList.add("hidden");
+            adventureLogHandler.appendSystemMessage("You successfully unlocked the door.");
+            dungeonDescription.textContent = "You found a " + level.type + " called " + level.name + ". It's unlocked. Do you wish to enter?";
+        } else {
+            adventureLogHandler.appendSystemMessage("You failed to unlock the door.");
+        }
+    }
+
+    bashBtn.onclick = () => {
+        let isBashed = bashDoor(gameData.playerCharacteristics.might, level.isLocked);
+        if (isBashed) {
+            level.isLocked = "";
+            yesBtn.classList.remove("hidden");
+
+            unlockBtn.classList.add("hidden");
+            bashBtn.classList.add("hidden");
+            adventureLogHandler.appendSystemMessage("You bashed the door with all your might.");
+            dungeonDescription.textContent = "You found a " + level.type + " called " + level.name + ". The door is destroyed. Do you wish to enter?";
+        } else {
+            adventureLogHandler.appendSystemMessage("You are too weak to bash this door.");
+        }
+    }
 }
